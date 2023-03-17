53bd20d6887651b3b81370c4f97f3d89de8bc49c
DarklyZ <daniil.gorbik@gmail.com>
2023-03-17 14:34:46 +0500

    x and y coords - constructor params in MapSlot

--- src/main/java/net/fabricmc/example/mixin/PlayerScreenHandlerMixin.java
+++ src/main/java/net/fabricmc/example/mixin/PlayerScreenHandlerMixin.java
@@ -1,5 +1,6 @@
 package net.fabricmc.example.mixin;
 
+import net.fabricmc.example.drawables.MapSlotsWidget;
 import net.fabricmc.example.utils.MapSlot;
 import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.screen.AbstractRecipeScreenHandler;
@@ -9,7 +10,9 @@ import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(PlayerScreenHandler.class)
 public abstract class PlayerScreenHandlerMixin extends AbstractRecipeScreenHandler<CraftingInventory> {
-    private final MapSlot mapSlot = (MapSlot)this.addSlot(new MapSlot());
+    private final MapSlot mapSlot = (MapSlot)this.addSlot(new MapSlot(
+            -MapSlotsWidget.getWidth()/2-10, MapSlotsWidget.getHeight()/2-8
+    ));
 
     public PlayerScreenHandlerMixin(ScreenHandlerType<?> screenHandlerType, int i) {
         super(screenHandlerType, i);
--- src/main/java/net/fabricmc/example/utils/MapSlot.java
+++ src/main/java/net/fabricmc/example/utils/MapSlot.java
@@ -1,12 +1,11 @@
 package net.fabricmc.example.utils;
 
-import net.fabricmc.example.drawables.MapSlotsWidget;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.screen.slot.Slot;
 
 public class MapSlot extends Slot {
-    public MapSlot() {
-        this(new MapSlotsInv(), 0, -MapSlotsWidget.getWidth()/2-10, MapSlotsWidget.getHeight()/2-8);
+    public MapSlot(int x, int y) {
+        this(new MapSlotsInv(), 0, x, y);
     }
 
     public MapSlot(Inventory inventory, int index, int x, int y) {
